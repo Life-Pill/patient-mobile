ad79610334a9019f0e890687c750d20932dad8cc
ChandulaJ <chandugoods3000@gmail.com>
2024-04-28 23:48:50 +0530

    Add http package to dependencies in pubspec.yaml

--- lib/features/Data/profile_data.dart
+++ lib/features/Data/profile_data.dart
@@ -1,17 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 
 class Profile {
-  String name = "John Red";
-  String email = "john@gmail.com";
+  String name = "default";
+  String email = "";
   String img = "assets/images/defaultProfilePic.png";
-  String dob = "11/02/2020";
+  String dob = "";
   String gender = "male";
-  String address = "No 2, ABC road, ABC";
-  String phoneNumber = "07171717171";
+  String address = "";
+  String phoneNumber = "";
   List<String> reports = [];
   List<String> prescriptions = [];
 }
-
 
 class ProfileController extends GetxController {
   var currentUser = Profile().obs;
@@ -33,6 +34,36 @@ class ProfileController extends GetxController {
       val?.phoneNumber = phoneNumber;
     });
   }
+
+  Future<void> fetchProfileData() async {
+    try {
+      final response =
+          await http.get(Uri.parse('http://192.168.50.227:8080/customers/1'));
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        updateProfile(
+          name: jsonData['customerFullName'],
+          email: jsonData['customerEmail'],
+          dob: '', // Fill with appropriate value if available in your API
+          gender: 'male', // Fill with appropriate value if available in your API
+          address: jsonData['customerAddressStreet'] +
+              ', ' +
+              jsonData['customerAddressCity'] +
+              ', ' +
+              jsonData['customerAddressDistrict'],
+          phoneNumber: jsonData['customerMobileNumber'],
+        );
+          print('Name: ${currentUser.value.name}');
+        print('Email: ${currentUser.value.email}');
+        print('Date of Birth: ${currentUser.value.dob}');
+        print('Gender: ${currentUser.value.gender}');
+        print('Address: ${currentUser.value.address}');
+        print('Phone Number: ${currentUser.value.phoneNumber}');
+      } else {
+        throw Exception('Failed to load profile data');
+      }
+    } catch (error) {
+      throw Exception('Failed to load profile data: $error');
+    }
+  }
 }
-
-
