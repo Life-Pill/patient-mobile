ec7084289769732fcb7ba4a85cc8d1ded022dd9f
ChandulaJ <chandugoods3000@gmail.com>
2024-06-05 16:35:06 +0530

    added a image to display when cart is empty

--- lib/features/main_screens/cart_tab/cart_tab_screen.dart
+++ lib/features/main_screens/cart_tab/cart_tab_screen.dart
@@ -4,7 +4,6 @@ import 'package:patientmobileapplication/features/main_screens/cart_tab/componen
 import 'package:patientmobileapplication/features/main_screens/cart_tab/components/cart_tab_tile.dart';
 import 'package:patientmobileapplication/features/main_screens/components/top_navbar.dart';
 
-
 class CartTabScreen extends StatelessWidget {
   final CartController cartController = Get.put(CartController());
 
@@ -15,39 +14,55 @@ class CartTabScreen extends StatelessWidget {
       body: Column(
         children: [
           TopNavBar(
-                NeedSearchBar: false,
-                TabName: "Your Cart",
-              ),
+            NeedSearchBar: false,
+            TabName: "Your Cart",
+          ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-              child: Obx(() => ListView.builder(
+              padding: const EdgeInsets.only(left: 5.0, right: 10.0),
+              child: Obx(() {
+                if (cartController.cartItems.isEmpty) {
+                  return Center(
+                    child: Image.asset(
+                      'assets/images/empty-cart.png',
+                      width: 200,
+                      height: 200,
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
                     itemCount: cartController.cartItems.length,
                     itemBuilder: (context, index) {
                       return CartTabTile(
                         item: cartController.cartItems[index],
                       );
                     },
-                  )),
+                  );
+                }
+              }),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20.0, bottom: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Obx(
-                  () => Text(
-                    "Total: Rs. ${cartController.totalBill.toStringAsFixed(2)}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
+          Obx(() {
+            if (cartController.cartItems.isNotEmpty) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 20.0, bottom: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Total: Rs. ${cartController.totalBill.toStringAsFixed(2)}",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              );
+            } else {
+              return Container();
+            }
+          }),
           Obx(
             () => ElevatedButton(
               style: ElevatedButton.styleFrom(
