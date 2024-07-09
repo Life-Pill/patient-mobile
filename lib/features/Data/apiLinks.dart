c08fcb320a41802944edacfa449ac0323366611a
Chandula Jayathilake <114409648+ChandulaJ@users.noreply.github.com>
2024-07-09 10:06:06 +0530

    Update apiLinks.dart

--- lib/features/Data/apiLinks.dart
+++ lib/features/Data/apiLinks.dart
@@ -1,6 +1,6 @@
-const String CustomerDetailsAPI = 'http://52.200.74.128/customers/1';
-const String CustomerPrescriptionsAPI = 'http://52.200.74.128/prescriptionImages/1';
-const String medicineSearch = 'http://35.174.112.238/lifepill/v1/medicine-finding/find-medicine?itemName=';
-const String CreateNewCustomer = 'http://52.200.74.128/customers';
+const String CustomerDetailsAPI = 'http://10.0.2.2:8075/customers/1';
+const String CustomerPrescriptionsAPI = 'http://10.0.2.2:8075/prescriptionImages/1';
+const String medicineSearch = 'http://10.0.2.2:8079/lifepill/v1/medicine-finding/find-medicine?itemName=';
+const String CreateNewCustomer = 'http://10.0.2.2:8075/customers';
 const int ConstCustomerID = 1;
-const String PrescriptionOrderPostAPI = 'http://52.200.74.128/myOrders';
+const String PrescriptionOrderPostAPI = 'http://10.0.2.2:8075/myOrders';
